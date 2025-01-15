a2a32fdfd4d2985775a3a66fba1a1fe1c179fb3a
devJiraynor <jiraynor@naver.com>
2025-01-15 16:45:33 +0900

    2024.01.15. 16:50

--- DDL_example.sql
+++ DDL_example.sql
@@ -58,3 +58,42 @@ CREATE USER 'school_developer'@'localhost' IDENTIFIED BY 'school123';
 -- 선수 [이성계, 1998-01-15, 공격수, 10, 대한민국]
 -- 경기장 [한양메인스타디움, 대한민국 서울특별시 강남구, 2000]
 -- 심판 [이방원, 1994-06-05, 대한민국, 주심]
+
+-- 1. 축구 경기 데이터베이스 생성
+CREATE DATABASE football;
+-- 2. 축구 경기 데이터베이스 선택
+USE football;
+-- 3. 참가팀 테이블 생성
+CREATE TABLE team (
+    nation VARCHAR(60),
+    seed VARCHAR(3),
+    manager VARCHAR(100),
+    lanking INT
+);
+-- 4. 선수 테이블 생성
+CREATE TABLE player (
+    name VARCHAR(100),
+    birth VARCHAR(10),
+    position VARCHAR(12),
+    uniform_number INT,
+    country VARCHAR(60)
+);
+-- 5. 경기장 테이블 생성
+CREATE TABLE stadium (
+    name VARCHAR(255),
+    address TEXT,
+    seats INT
+);
+-- 6. 심판 테이블 생성
+CREATE TABLE referee (
+    name VARCHAR(100),
+    birth VARCHAR(10),
+    country VARCHAR(60),
+    position VARCHAR(6)
+);
+-- 7. 계정 생성
+CREATE USER 'football_developer'@'%' IDENTIFIED BY 'foot!@';
+CREATE USER 'football_broadcast'@'%' IDENTIFIED BY 'foot#$';
+-- 8. 심판과 선수의 birth 컬럼의 데이터 타입을 DATE로 변경
+ALTER TABLE player MODIFY COLUMN birth DATE;
+ALTER TABLE referee CHANGE birth birth DATE;
