70e5fbf5dd844386074ed18a006ae948781fff37
devJiraynor <jiraynor@naver.com>
2025-01-17 11:49:08 +0900

    2024.01.17. 11:50

--- Constraints.sql
+++ Constraints.sql
@@ -48,3 +48,58 @@ UPDATE unique_table SET unique_column = 1;
 -- 기본키 (Primary Key) : 후보키에서 프로세스에 맞게 선택된 레코드를 구분할 수 있는 키
 -- 대체키 (Alternate Key) : 후보키에서 기본키로 선택되지 않은 나머지 키
 -- 복합키 (Composite Key) : 두 개 이상의 컬럼의 조합으로 레코드를 구분할 수 있는 기본키
+
+-- PRIMARY KEY 제약조건 : 해당 컬럼을 기본키로 지정하는 제약
+-- (NOT NULL + UNIQUE)
+-- [INSERT, UPDATE]
+CREATE TABLE pk_table (
+    primary_column INT PRIMARY KEY,
+    other_column INT NOT NULL UNIQUE
+);
+
+-- PRIMARY KEY 제약조건은 NOT NULL과 UNIQUE 제약조건을 모두 가지고 있음
+INSERT INTO pk_table VALUES (null, 1);
+INSERT INTO pk_table (other_column) VALUES (2);
+
+INSERT INTO pk_table VALUES (1, 1);
+INSERT INTO pk_table VALUES (1, 2);
+
+-- PRIMARY KEY 제약조건을 두 개 이상 지정 불가능
+CREATE TABLE composite_table (
+    pk1 INT PRIMARY KEY,
+    pk2 INT PRIMARY KEY
+);
+
+-- 제약조건 지정 방법
+-- CONSTRAINT 제약조건의이름 제약조건 (선택할 컬럼)
+CREATE TABLE composite_table (
+    pk1 INT,
+    pk2 INT,
+    CONSTRAINT composite_table_pk PRIMARY KEY (pk1, pk2)
+);
+
+-- 제약조건 수정
+-- 1. ALTER 테이블명 MODIFY COLUMN 컬럼명 데이터타입 제약조건
+-- 2. ALTER 테이블명 DROP CONSTRAINT 제약조건이름
+--    ALTER 테이블명 ADD CONSTRAINT 제약조건이름
+
+-- 주의 : 제약조건 변경시 실제 데이터가 유효한지 검증을 먼저 수행해야함
+SELECT * FROM not_null_table;
+
+UPDATE not_null_table SET null_column = 1 
+WHERE null_column IS NULL;
+
+ALTER TABLE not_null_table 
+MODIFY COLUMN null_column INT NOT NULL;
+
+ALTER TABLE not_null_table
+ADD CONSTRAINT not_null_table_uq UNIQUE (null_column);
+
+-- FOREIGN KEY 제약조건 : 특정 컬럼을 다른 테이블 혹은 같은 테이블의 기본키 컬럼과 연결하는 제약
+-- FOREIGN KEY 제약조건이 지정되는 컬럼은 참조하고자하는 컬럼의 데이터 타입과 일치해야함
+CREATE TABLE fK_table (
+    pk_column INT PRIMARY KEY,
+    fk_column INT,
+    CONSTRAINT fk_table_fk FOREIGN KEY (fk_column)
+    REFERENCES pk_table(primary_column)
+);
